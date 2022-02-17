8e4f10f9019ad0ea007c99263112e6d547f7faa6
ankamsarav <ankamsarav@gmail.com>
2022-02-17 11:53:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function shadowWizard(options){
+    let images = document.querySelectorAll('.shadowWizard');
+    if(options.shadowType === 'hard'){
+        options.shadowType = '0px'
+    }
+    else {
+        options.shadowType = '10px'
+    }
+    images.forEach((image)=>{
+        image.style.boxShadow = `10px 10px ${options.shadowType} 1px rgba(0,0,0,0.12)`;
+
+        if(options.padding) {
+            image.style.padding = '10px'
+        }
+    });
+    
+}
+
+module.exports.ShadowWizard = ShadowWizard;
